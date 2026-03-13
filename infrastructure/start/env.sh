495b35a0af32b0ff91f0f8430f3940270b086e64
CVSz <seaza@msn.com>
2026-03-13 20:23:57 +0700

    Fix startup DB connectivity defaults and migration target

--- infrastructure/start/env.sh
+++ infrastructure/start/env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 export DB_URL=postgresql://zttato:zttato@localhost:5432/zttato
+export CONTAINER_DB_URL=postgresql://zttato:zttato@postgres:5432/zttato
 export REDIS_URL=redis://localhost:6379
+export CONTAINER_REDIS_URL=redis://redis:6379
 export GPU_RENDER=true
